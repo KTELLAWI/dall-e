2c6ac0a79ac7b1190d0ed9412113e2c7f64a4b65
KTELLAWI <koutaiba@msn.com>
2023-02-28 20:09:53 +0300

    add docker file

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -33,7 +33,7 @@ function Home() {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://generateimage-ki6q.onrender.com//api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
